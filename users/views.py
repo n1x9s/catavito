55007a90e17289b47002ff55b9f8f240484bdaaf
spl1ntero <sernekit77@gmail.com>
2024-04-17 09:29:20 +0300

    commit

--- users/views.py
+++ users/views.py
@@ -23,9 +23,8 @@ def sign_in(request):
             if user:
                 login(request, user)
                 messages.success(request, f'Hi {username.title()}, welcome back!')
-                return redirect('posts')
+                return redirect('index')
 
-        # form is not valid or user is not authenticated
         messages.error(request, f'Invalid username or password')
         return render(request, 'users/login.html', {'form': form})
 
