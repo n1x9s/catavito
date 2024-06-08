b5fb96e78ed4b4e4255e400c72b5f34ead314925
niki <113206190+n1x9s@users.noreply.github.com>
2024-06-08 04:10:19 +0300

    Delete catshop directory

--- catshop/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Cat
-
-
-@admin.register(Cat)
-class CatAdmin(admin.ModelAdmin):
-    list_display = ('image', 'name', 'description', 'price')
--- catshop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('cats/<int:cat_id>/', views.show_cat, name='show_cat'),
-    path('about/', views.about, name='about'),
-    path('cats/<int:cat_id>/buy/', views.buy_cat, name='buy_cat'),
-]
